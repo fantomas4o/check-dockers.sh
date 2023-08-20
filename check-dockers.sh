9a702683488aaf21808291a0d905a6babdfd2528
fantomas4o <142707573+fantomas4o@users.noreply.github.com>
2023-08-20 08:41:12 +0300

    Create check-dockers.sh

---
+++ check-dockers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+TOKEN=""
+chat_id=""
+FILE=proverka-konteineri
+_dockerlist=$(docker ps -a --format "table {{.Names}}" | tail -n +2)
+_zapylnene=""
+#for i in $_dockerlist; do docker container inspect -f '{{.State.Status}}' $i; done
+for z in $_dockerlist; do
+	eee=$(docker container inspect -f '{{.State.Status}}' $z)
+	if [[ "$eee" != "running" ]]; then
+		if [[ $z == *_* ]]; then
+			z=${z//_/-}
+		fi
+		_zapylnene+="$z"
+		_zapylnene+=" "
+	fi
+done	
+if [[ -n $_zapylnene ]]; then
+        echo 0 > ${FILE}
+else
+        echo 1 > ${FILE}
+fi
+down=`grep -ic "1" ${FILE}`
+if [ $down -eq 1 ]; then
+  if [ -f ${FILE}.txt ]; then
+    textt="🟢 *Great! All containers are working!*%0A%0A"
+    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id=${chat_id} -d text="${textt}" -d parse_mode=Markdown
+    rm -rf ${FILE}.txt
+  fi
+else
+  if [ -f ${FILE}.txt ]; then
+   echo "$tt" >> ${FILE}.txt
+  else
+    textt="🔴 *Stopped containers:*%0A%0A"
+    textt+=$(echo $_zapylnene | tr ' ' '\n')
+    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id=${chat_id} -d text="${textt}" -d parse_mode=Markdown
+    echo "$tt" > ${FILE}.txt
+  fi
+fi
